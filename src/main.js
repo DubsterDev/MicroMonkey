5895721e6f1ea5d7fa119ab4a49700d67f93e8ff
Dubster <dubster@localhost>
2025-08-21 12:19:36 -0500

    Delete default folder structure

--- src/main.js
+++ src/main.js
@@ -4,15 +4,4 @@ import { getFiles, startSerial } from "./serial";
 import { newFolderStructure } from "./fileExplorer";
 const editor = setUpMonaco();
 
-newFolderStructure({
-    "main.py": "main.py",
-    "bob.py": "bob.py",
-    "fred.py": "fred.py",
-    "chatter2": {
-        "chat.py": "chat.py",
-        "libs": {
-            "get_french_fries.py": "get_french_fires.py"
-        }
-    }
-})
 startSerial(editor, async () => newFolderStructure(await getFiles()));
