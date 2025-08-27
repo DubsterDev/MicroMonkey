e9b59fe5aa0281108e230485792ce8b461132636
Dubster <dubster@localhost>
2025-08-27 16:27:45 -0500

    Improve command palette

--- src/commandPalette.js
+++ src/commandPalette.js
@@ -9,18 +9,30 @@ const commands = [
         "id": "alert",
         "name": "Say hi",
         "callback": () => alert("HI!")
+    },
+    {
+        "id": "deleteit",
+        "name": "Delete something",
+        "callback": async () => {
+            const input = await getInput("What would you like to delete?");
+            removeCommand(input)
+        }
+    },
+    {
+        "id": "addit",
+        "name": "Add say hi",
+        "callback": () => addCommand("alert2", "Say hi (added)", () => alert("Howdy"))
     }
 ];
+
+let usingCommands = commands;
+let callbackEnter, callbackEscape;
 
 export function setupCommandPalette() {
     document.addEventListener("keydown", (ev) => {
         if (ev.ctrlKey && ev.shiftKey && ev.key.toLowerCase() === "p") {
             ev.preventDefault();
-            commandPalette.style.display = "block";
-            commandPaletteInput.value = "";
-            commandPaletteInput.placeholder = "";
-            commandPaletteInput.focus();
-            displaySuggestions();
+            showCommandPalette();
             return true;
         }
     });
@@ -29,6 +41,14 @@ export function setupCommandPalette() {
         if (ev.key.toLowerCase() === "escape") {
             ev.preventDefault();
             commandPalette.style.display = "none";
+            if (callbackEscape) callbackEscape();
+            return true;
+        } else if (ev.key.toLowerCase() === "enter") {
+            ev.preventDefault();
+            if (callbackEnter) {
+                commandPalette.style.display = "none";
+                callbackEnter(commandPaletteInput.value);
+            }
             return true;
         }
 
@@ -36,18 +56,57 @@ export function setupCommandPalette() {
     });
 }
 
+export function showCommandPalette(placeholder="", defaultValue="", useCommands=commands, optionPicked, escaped) {
+    commandPalette.style.display = "block";
+    commandPaletteInput.value = defaultValue;
+    commandPaletteInput.placeholder = placeholder;
+    commandPaletteInput.focus();
+    usingCommands = useCommands;
+    callbackEnter = optionPicked;
+    callbackEscape = escaped;
+
+    displaySuggestions();
+}
+
+export function getInput(placeholder, defaultValue) {
+    return new Promise((resolve, reject) => {
+        showCommandPalette(placeholder, defaultValue, [], resolve, reject);
+    })
+}
+
 export function addCommand(id, name, callback) {
-    commands.push({
+    const command = {
         "id": id,
         "name": name,
         "callback": callback
+    };
+
+    let needsToAddNewCommand = true;
+    commands.forEach((_command, index) => {
+        if (_command.id === id) {
+            needsToAddNewCommand = false;
+            commands[index] = command;
+        }
     });
+
+    if (needsToAddNewCommand) commands.push(command);
+
+    displaySuggestions();
+}
+
+export function removeCommand(id) {
+    commands.forEach((command, index) => {
+        if (command.id === id) {
+            commands.splice(index, 1);
+        }
+    });
+    displaySuggestions();
 }
 
 function displaySuggestions(searchTerm="") {
     commandPaletteSuggestions.innerText = "";
 
-    const fuse = new Fuse(commands, {
+    const fuse = new Fuse(usingCommands, {
         keys: [
             "id",
             "name"
@@ -56,7 +115,7 @@ function displaySuggestions(searchTerm="") {
 
     console.log(searchTerm);
 
-    const searchResults = searchTerm.trim() !== "" ? fuse.search(searchTerm) : commands;
+    const searchResults = searchTerm.trim() !== "" ? fuse.search(searchTerm) : usingCommands;
 
     searchResults.forEach((item) => {
         const { name, id, callback } = "item" in item ? item.item : item;
