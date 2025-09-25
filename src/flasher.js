c84e000ded9b655f57515aa3c3c0dd62cee6b770
Dubster <dubster@localhost>
2025-09-24 19:30:06 -0500

    Add code to connect to the board with esptool

--- src/flasher.js
+++ src/flasher.js
@@ -1,7 +1,8 @@
 import { Transport, ESPLoader } from "esptool-js";
-import { openTab } from "./openFilesManager";
-import { addHeading, addMessageBox, addParagraph } from "./customEditorHelperFunctions";
+import { openTab, requestReRender } from "./openFilesManager";
+import { addButton, addHeading, addMessageBox, addParagraph } from "./customEditorHelperFunctions";
 import { addCommand } from "./commandPalette";
+import { disconnectSerialPort, disconnectSerialPortListeners, getPort } from "./serial";
 
 const espLoaderTerminal = {
   clean() {
@@ -15,16 +16,49 @@ const espLoaderTerminal = {
   }
 }
 
+const myFilePath = "/.default_files/micromonkey/flasher.mm";
+
 export function addFlasherEventListeners() {
     document.getElementById("flashMicroPython").addEventListener("click", openFlasher)
     addCommand("openMicroPythonFlasher", "Flash MicroPython", openFlasher);
 }
 
+let device;
+let transport;
+let flashingStage = "not_started";
+let esploader = null
+let chip = null;
+async function beginFlash() {
+    await disconnectSerialPort();
+    device = getPort();
+    transport = new Transport(device, true);
+
+    flashingStage = "getting_ready";
+
+    requestReRender(myFilePath);
+    const flashOptions = {
+      transport,
+      baudrate: 460800,
+      terminal: espLoaderTerminal,
+      debugLogging: true,
+    };
+    esploader = new ESPLoader(flashOptions);
+    chip = await esploader.main();
+
+}
+
 function openFlasher() {
-    openTab("/.default_files/micromonkey/flasher.mm", "MicroPython Flasher", "custom", renderFlasherPage);
+    openTab(myFilePath, "MicroPython Flasher", "custom", renderFlasherPage);
 }
 
 function renderFlasherPage(root) {
     addHeading("MicroPython Flasher", "h2", root);
-    addMessageBox("warning", "Currently this flasher only supports Espressif boards, such as the ESP32 or ESP8266.", root);
+    if (flashingStage === "not_started") {
+        addMessageBox("warning", "Currently this flasher only supports Espressif boards, such as the ESP32 or ESP8266.", root);
+        addParagraph("Connect to a board with the button on the bottom left, then press continue.", root);
+        addParagraph("Press and hold the EN button on your board while pressing the continue button.", root);
+        addButton("Continue", root, beginFlash);
+    } else if (flashingStage === "getting_ready") {
+        addParagraph("Getting ready to flash MicroPython...", root);
+    }
 }
