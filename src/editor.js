560ec50b6a963ee668e9d5b7520fc530cd3bfd08
Dubster <dubster@localhost>
2025-09-26 15:23:08 -0500

    Skip unknown modules as well

--- src/editor.js
+++ src/editor.js
@@ -230,8 +230,8 @@ export function updateDiagnostics(diagnostics, uri) {
 
     // Loop through the array of diagnostics
     diagnostics.forEach(diagnostic => {
-        // Skip it if it is a missing module source
-        if (diagnostic.code === "reportMissingModuleSource") return;
+        // Skip it if it is a missing module source or unknown module
+        if (diagnostic.code === "reportMissingModuleSource" || diagnostic.code === "reportMissingImports") return;
         // Create a Monaco style diagnostic
         const newDiagnostic = {
             endColumn: diagnostic.range.end.character + 1,
