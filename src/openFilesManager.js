7a71b8f2ff84c1342dbf1c9919c9814df3ba02f9
Dubster <dubster@localhost>
2025-10-01 10:55:44 -0500

    Only scroll the active tab into view if it's not already in view

--- src/openFilesManager.js
+++ src/openFilesManager.js
@@ -260,5 +260,16 @@ function renderTabs(activateActiveTab=true) {
     });
 
     // Scroll the active tab into view
-    document.querySelector(".tabs .tab.active")?.scrollIntoView({behavior: "smooth", block: "nearest", inline: "center"});
+    const activeTab = document.querySelector(".tabs .tab.active");
+    if (activeTab !== null && activeTab !== undefined && !isInViewport(activeTab)) activeTab?.scrollIntoView({behavior: "instant", block: "nearest", inline: "nearest"});
+}
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
