749981809b9fae37bdcdfad4bf82919fb16230f4
Dubster <dubster@localhost>
2025-10-01 22:57:35 -0500

    Seperate logic for flashing

---
+++ src/espFlasher.js
@@ -0,0 +1,114 @@
+// Import dependencies from esptool-js and spark-md5
+import { ESPLoader, Transport } from "esptool-js";
+import SparkMD5 from "spark-md5";
+
+// A simple terminal object to log messages from esptool-js to the console
+const espLoaderTerminal = {
+  clean() {
+    console.clear()
+  },
+  writeLine(data) {
+    console.log(data)
+  },
+  write(data) {
+    console.log(data)
+  }
+}
+
+// Variables to hold the transport, esploader, and chip objects
+let transport;
+let esploader = null;
+let chip = null;
+
+/**
+ * Starts flashing an Espressif device with the provided firmware file.
+ * @param {*} device The serial port to flash
+ * @param {Function} setFlashingStage A callback used to update the flashing stage. Called with one parameter, the stage as a string
+ * @param {Function} setFlashingProgress A callback used to update the flashing progress. Called with two parameters, total amount to flash and amount that has been flashed
+ * @param {File} file The firmware file to flash
+ */
+export async function flashESP(device, setFlashingStage, setFlashingProgress, file) {
+    // Create a new transport object for the serial port
+    transport = new Transport(device, true);
+    
+    // Update the flashing stage to "getting_ready"
+    setFlashingStage("getting_ready");
+
+    // Define some flashing options
+    const flashOptions = {
+        transport,
+        baudrate: 460800,
+        terminal: espLoaderTerminal,
+        debugLogging: true,
+    };
+
+    // Create a new esploader object with the flashing options
+    esploader = new ESPLoader(flashOptions);
+    
+    // Detect the chip
+    chip = await esploader.main();
+
+    // Start flashing MicroPython
+    flashOS(setFlashingStage, setFlashingProgress, file);
+}
+
+/**
+ * Flash the file to the device using esptool-js
+ * @param {Function} setFlashingStage A callback used to update the flashing stage, called with one parameter, the stage as a string
+ * @param {Function} setFlashingProgress A callback used to update the flashing progress, called with two parameters, total amount to flash and amount that has been flashed
+ * @param {File} file The firmware file to flash
+ */
+async function flashOS(setFlashingStage, setFlashingProgress, file) {
+    // Update the flashing stage to "flashing"
+    setFlashingStage("flashing");
+
+    // Erase the flash memory
+    await esploader.eraseFlash();
+
+    // Load the firmware file into a binary string
+    const osString = await loadOSIntoString(file);
+
+    // Create a file array with the firmware data and the address to flash it to
+    const fileArray = [{ data: osString, address: parseInt("0x1000") }];
+
+    // Create the flash options
+    const flashOptions = {
+        fileArray: fileArray,
+        flashSize: "keep",
+        eraseAll: false,
+        compress: true,
+        reportProgress: (fileIndex, written, total) => {
+            setFlashingProgress(total, written);
+        },
+        calculateMD5Hash: (image) => SparkMD5.hashBinary(image),
+    }
+
+    // Write the flash and finalize
+    await esploader.writeFlash(flashOptions);
+    await esploader.after();
+
+    // Update the flashing stage to "done"
+    setFlashingStage("done");
+}
+
+
+/**
+ * Loads a file into a binary string.
+ * @param {File} file The file to load into a binary string
+ * @returns {Promise<string>} A promise that resolves to the binary string of the file
+ */
+function loadOSIntoString(file) {
+    return new Promise((resolve) => {
+        // Create a new FileReader to read the file
+        const micropythonReader = new FileReader();
+
+        micropythonReader.onload = (ev) => {
+            // When the file is loaded, resolve the promise with the binary string
+            resolve(ev.target.result);
+        }
+
+        // Read the file as a binary string
+        // TODO: Modify this to use a non-deprecated method
+        micropythonReader.readAsBinaryString(file);
+    })
+}
--- src/flasher.js
+++ src/flasher.js
@@ -1,121 +1,128 @@
-import { Transport, ESPLoader } from "esptool-js";
+// Import necessary functions and modules
 import { openTab, requestReRender } from "./openFilesManager";
 import { addButton, addHeading, addMessageBox, addParagraph } from "./customEditorHelperFunctions";
-import { addCommand } from "./commandPalette";
-import { connectToBoard, disconnectSerialPort, disconnectSerialPortListeners, getPort } from "./serial";
-import SparkMD5 from "spark-md5";
+import { addCommand, getInput } from "./commandPalette";
+import { disconnectSerialPort, getPort } from "./serial";
+import { flashESP } from "./espFlasher";
 
-const espLoaderTerminal = {
-  clean() {
-    console.clear()
-  },
-  writeLine(data) {
-    console.log(data)
-  },
-  write(data) {
-    console.log(data)
-  }
-}
+// Define the board type, defined later when selected by user
+let boardType = null;
 
+// The file path used for the tab
 const myFilePath = "/.default_files/micromonkey/flasher.mm";
 
+// Variables to track flashing progress
 let flashingTotal = 0;
 let flashingAmountDone = 0;
 
+// Create a hidden file input element for firmware upload
 const fileInput = document.createElement("input");
 fileInput.type = "file";
+fileInput.accept = ".bin";
 fileInput.onchange = () => requestReRender(myFilePath);
 
+/**
+ * Add event listeners for flasher functionality, such as the button on the bottom right of the IDE and the command in the command palette.
+ */
 export function addFlasherEventListeners() {
+    // Add an event listener to the button on the bottom right of the IDE
     // document.getElementById("flashMicroPython").addEventListener("click", openFlasher)
+
+    // Add a command to the command palette
     addCommand("openMicroPythonFlasher", "Flash MicroPython", openFlasher);
 }
 
-let device;
-let transport;
+// The stage of flashing, used to determine what to show the user
 let flashingStage = "not_started";
-let esploader = null
-let chip = null;
+
+/**
+ * Begin the flashing process after the user has selected a file and connected to a device.
+ */
 async function beginFlash() {
+    // Disconnect the serial port from serial.js
     await disconnectSerialPort();
-    device = getPort();
-    transport = new Transport(device, true);
 
-    flashingStage = "getting_ready";
+    // Get the serial port from serial.js
+    const device = getPort();
 
-    requestReRender(myFilePath);
-    const flashOptions = {
-      transport,
-      baudrate: 460800,
-      terminal: espLoaderTerminal,
-      debugLogging: true,
-    };
-    esploader = new ESPLoader(flashOptions);
-    chip = await esploader.main();
-    flashOS();
+    if (boardType === "espressif") {
+        // If the board type is an espressif device, flash it using the espFlasher.js module
+        // Passing the serial port, function to update the flashing stage,
+        // a function to update the flashing progress, and the file to flash
+        flashESP(device, (stage) => {
+            flashingStage = stage;
+            requestReRender(myFilePath);
+        }, (total, done) => {
+            flashingTotal = total;
+            flashingAmountDone = done;
+            requestReRender(myFilePath);
+        }, fileInput.files[0]);
+    } 
 }
 
-async function flashOS() {
-    flashingStage = "flashing";
-    requestReRender(myFilePath);
-    await esploader.eraseFlash();
-    const osString = await loadOSIntoString();
+/**
+ * Opens the flasher tab after asking the user what type of board they are trying to flash.
+ */
+async function openFlasher() {
+    // Get what type of board the user is trying to flash
+    const flasherType = await getInput("What type of board are you trying to flash?", "Filter results", "", ["ESP32 or ESP8266", "Cancel"], false);
 
-    const fileArray = [{ data: osString, address: parseInt("0x1000") }];
-    const flashOptions = {
-        fileArray: fileArray,
-        flashSize: "keep",
-        eraseAll: false,
-        compress: true,
-        reportProgress: (fileIndex, written, total) => {
-            flashingTotal = total;
-            flashingAmountDone = written;
-            requestReRender(myFilePath);
-        },
-        calculateMD5Hash: (image) => SparkMD5.hashBinary(image),
+    // If the user cancels, do nothing
+    if (flasherType === "Cancel") {
+        return;
     }
 
-    await esploader.writeFlash(flashOptions);
-    await esploader.after();
+    if (flasherType === "ESP32 or ESP8266") {
+        // If the user is trying to flash an ESP32 or ESP8266, set the board type to espressif
+        boardType = "espressif";
+    } else {
+        // If there is no valid selection, do nothing
+        return;
+    }
 
-    flashingStage = "done";
-    requestReRender(myFilePath);
-}
-
-function loadOSIntoString() {
-    return new Promise((resolve) => {
-        const micropythonReader = new FileReader();
-        micropythonReader.onload = (ev) => {
-            resolve(ev.target.result);
-        }
-
-        micropythonReader.readAsBinaryString(fileInput.files[0]);
-    })
-}
-
-function openFlasher() {
+    // Open the flasher tab
     openTab(myFilePath, "MicroPython Flasher", "custom", renderFlasherPage);
 }
 
+/**
+ * Renders the flasher page based on the current flashing stage.
+ * @param {Element} root The element to render the flasher page into
+ */
 function renderFlasherPage(root) {
+    // Add the heading
     addHeading("MicroPython Flasher", "h2", root);
+
     if (flashingStage === "not_started") {
-        addMessageBox("warning", "Currently this flasher only supports Espressif boards, such as the ESP32 or ESP8266.", root);
+        // If the flashing process has not started, show the user the file input and instructions
+        if (boardType === "espressif") {
+            // A warning message to ensure the user is using the right type of device
+            addMessageBox("warning", "This flasher is for flashing Espressif devices, such as the ESP32 or ESP8266. If this is not the right type of device, reopen the flasher", root);
+        }
+
+        // The file input for the user to upload the firmware
         addParagraph(fileInput.files.length > 0 ? "Firmware selected" : "You must upload the firmware to flash.", root);
         addButton(fileInput.files.length > 0 ? "Change" : "Upload", root, () => fileInput.click())
+
+        // Explanatory text and button to begin the flashing process
         addParagraph("Connect to a board with the button on the bottom left, then press continue.", root);
         addParagraph("Press and hold the Boot button on your board while pressing the continue button.", root);
+
+        /// The button to begin the flashing process
         addButton("Continue", root, beginFlash);
     } else if (flashingStage === "getting_ready") {
+        // If the flashing process is getting ready, show a message
         addParagraph("Getting ready to flash MicroPython...", root);
     } else if (flashingStage === "flashing") {
+        // If the flashing process is in progress, show a message and a progress bar
         addParagraph("Flashing MicroPython...", root);
 
+        // The progress bar showing the flashing progress
         const percentHolder = document.createElement("progress");
         percentHolder.max = flashingTotal;
         percentHolder.value = flashingAmountDone;
         root.appendChild(percentHolder);
     } else if (flashingStage === "done") {
+        // If the flashing process is done, show a message and a button to refresh the IDE
         addParagraph("Done flashing.", root);
         addParagraph("Reboot your device by pressing the EN button.", root);
         addParagraph("Then, refresh MicroMonkey and reconnect.", root);
