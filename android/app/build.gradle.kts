208d4ef1ad1593a823da336123aab9662dfc4338
Dubster <dubster@localhost>
2025-11-21 12:59:51 -0600

    Update version numbers in other spots to match

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 121
-        versionName = "1.2.1-beta"
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
