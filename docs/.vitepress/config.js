59ba3af5eda18087a2e8753cfd3ad7b2fdafb3d2
DubsterDev <317384959+DubsterDev@users.noreply.github.com>
2026-08-18 14:49:21 -0500

    [Docs] Add link to GitHub repository

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -37,9 +37,9 @@ export default defineConfig({
     notFound: {
       quote: "Sorry, this page could not be found."
     },
-    // socialLinks: [
-    //   { icon: 'github', link: 'https://github.com/FXZFun/micromonkey' }
-    // ]
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/DubsterDev/MicroMonkey' }
+    ]
   },
   cleanUrls: true,
   ignoreDeadLinks: true
